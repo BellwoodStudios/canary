c7192e19cfcc4558bcd6ac6f61f8130da47e6c5f
Sam MacPherson <xylom44@gmail.com>
2019-03-19 16:15:03 -0400

    fix for s3 uploads

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -16,11 +16,11 @@ export class UploadModule {
 					case 'memory': storage = multer.memoryStorage(); break;
 					case 'disk': storage = multer.diskStorage({ destination:cfg.storage.path }); break;
 					case 's3':
-						const s3 = new aws.S3();
 						aws.config.update({
 							accessKeyId: cfg.storage.accessKey,
 							secretAccessKey: cfg.storage.secretKey,
 						});
+						const s3 = new aws.S3();
 						storage = multerS3({
 							s3,
 							bucket: cfg.storage.bucket,
