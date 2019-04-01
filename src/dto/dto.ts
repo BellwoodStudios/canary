d181afff9b02911562fa5e5da0505a93c7459e11
Sam MacPherson <xylom44@gmail.com>
2019-04-01 16:42:01 -0400

    added pagination dto

--- src/dto/dto.ts
+++ src/dto/dto.ts
@@ -1,7 +1,22 @@
 /** Common DTOs */
 import { IsNumberString } from 'class-validator';
+import { ArgsType, Field, Int } from 'type-graphql';
 
+@ArgsType()
 export class FindOneDto {
+
 	@IsNumberString()
 	id:number;
+
+}
+
+@ArgsType()
+export class PaginationDto {
+
+	@Field(type => Int)
+	skip:number = 0;
+
+	@Field(type => Int)
+	take:number = 25;
+
 }
